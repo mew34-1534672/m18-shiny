3cd925eeb16b474869d8cc2d191a2ae78f3ae4ea
Mary Elizabeth Ward <mew34@uw.edu>
2017-05-16 14:10:41 -0700

    Completed exercise 3

--- demo-1/server.R
+++ demo-1/server.R
@@ -14,9 +14,10 @@ shinyServer(function(input, output) {
   output$distPlot <- renderPlot({
     x    <- faithful[, 2]  # Old Faithful Geyser data
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
+    #bins <- seq(min(x), max(x), length.out = 100 + 1)
     
     # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    hist(x, breaks = bins, col = 'orange', border = 'black')
   })
   
 })
