521eaaaec106e03ac45fd8e4d886b794afa86c5a
Mary Elizabeth Ward <mew34@uw.edu>
2017-05-16 15:00:23 -0700

    made code more readable

--- exercise-4/ui.R
+++ exercise-4/ui.R
@@ -1,6 +1,26 @@
 # ui.R
 library(shiny)
 library(plotly)
+scatter.panel <- tabPanel('Scatter', 
+                          # Add a titlePanel to your tab
+                          titlePanel('Population vs. Vote Power'),
+                          
+                          # Create a sidebar layout for this tab (page)
+                          sidebarLayout(
+                            
+                            # Create a sidebarPanel for your controls
+                            sidebarPanel(
+                              
+                              # Make a textInput widget for searching for a state in your scatter plot
+                              textInput('search', label="Find a State", value = '')
+                            ),
+                            
+                            # Create a main panel, in which you should display your plotly Scatter plot
+                            mainPanel(
+                              plotlyOutput('scatter')
+                            )
+                          )
+)
 shinyUI(navbarPage('Electoral College',
                    # Create a tab panel for your map
                    tabPanel('Map',
@@ -21,26 +41,7 @@ shinyUI(navbarPage('Electoral College',
                               )
                             )
                    ), 
-                   
+                   scatter.panel
                    # Create a tabPanel to show your scatter plot
-                   tabPanel('Scatter', 
-                            # Add a titlePanel to your tab
-                             titlePanel('Population vs. Vote Power'),
-                            
-                            # Create a sidebar layout for this tab (page)
-                              sidebarLayout(
-                              
-                              # Create a sidebarPanel for your controls
-                                sidebarPanel(
-                                
-                                # Make a textInput widget for searching for a state in your scatter plot
-                                  textInput('search', label="Find a State", value = '')
-                                ),
-                              
-                              # Create a main panel, in which you should display your plotly Scatter plot
-                              mainPanel(
-                                plotlyOutput('scatter')
-                              )
-                              )
-                   )      
+                         
 ))
